62e188cc335762fde0bc94ba14e092da6039116f
Bea Steers <bea.steers@gmail.com>
2024-08-27 19:09:56 -0400

    improve diagnostic meta

--- config/init.sql
+++ config/init.sql
@@ -6,24 +6,9 @@ CREATE TABLE data (
     inputPayload TEXT,
     payload TEXT,
     rayQueueCountCurrent INTEGER,
-    rayQueueCountBacklog INTEGER
+    rayQueueCountBacklog INTEGER,
+    responseTime INTEGER,
+    nReplicas INTEGER
 );
 
 select create_hypertable('data', 'time');
-
--- CREATE TABLE data2 (
---     time BIGINT,
---     route TEXT,
---     inputPayload text,
---     payload text,
---     rayQueueCountCurrent INTEGER,
---     rayQueueCountBacklog INTEGER
--- );
-
--- \copy data2 from 'data/output.csv'  WITH CSV HEADER;
-
--- -- TRUNCATE data;
--- INSERT INTO data (time, route, inputPayload, payload, rayQueueCountCurrent, rayQueueCountBacklog)
--- SELECT to_timestamp(time/1000.0), route, inputPayload, payload, rayQueueCountCurrent, rayQueueCountBacklog
--- FROM data2;
--- drop table data2;
